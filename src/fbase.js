e8421a97a85110e3e0630cc765c950056db49fff
sekisuri - GitHub <sekisuri@gmail.com>
2021-10-18 04:39:25 +0900

    chapter 05

--- src/fbase.js
+++ src/fbase.js
@@ -9,12 +9,12 @@ import "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCyNm47P9MnVWo4NehsTeW-bSSsMHFNBF8",
-    authDomain: "sekisuri-nwitter.firebaseapp.com",
-    projectId: "sekisuri-nwitter",
-    storageBucket: "sekisuri-nwitter.appspot.com",
-    messagingSenderId: "903952669295",
-    appId: "1:903952669295:web:321036ee33ad60da458b9a"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
   };
 
 // Initialize Firebase

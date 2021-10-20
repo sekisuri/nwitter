1c41b6f3a4fef40210d7c07cbcc6eea0f9fbfe9f
sekisuri - GitHub <sekisuri@gmail.com>
2021-10-21 05:58:11 +0900

    chapter 06

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,4 +1,4 @@
-import { dbService } from "fbase";
+import { dbService, storageService } from "fbase";
 import { useState } from "react";
 
 const Nweet = ({ nweetObj, isOwner }) => {
@@ -7,29 +7,27 @@ const Nweet = ({ nweetObj, isOwner }) => {
 
   const onDeleteClick = async () => {
     const ok = window.confirm("삭제하시겠습니까?");
-    console.log(ok);
+
     if (ok) {
-      console.log(nweetObj.id);
-      const data = await dbService.doc(`nweets/${nweetObj.id}`).delete();
-      console.log(data);
+     
+      await dbService.doc(`nweets/${nweetObj.id}`).delete();
+      if (nweetObj.attachmentUrl !== "")
+        await storageService.refFromURL(nweetObj.attachmentUrl).delete();
     }
   };
 
   const toggleEditing = () => setEditing((prev) => !prev);
-
   const onChange = (event) => {
     const {
       target: { value },
     } = event;
     setNewNweet(value);
   };
-
   const onSubmit = async (event) => {
     event.preventDefault();
     await dbService.doc(`nweets/${nweetObj.id}`).update({ text: newNweet });
     setEditing(false);
   };
-
   return (
     <div>
       {editing ? (
@@ -43,6 +41,9 @@ const Nweet = ({ nweetObj, isOwner }) => {
       ) : (
         <>
           <h4>{nweetObj.text}</h4>
+          {nweetObj.attachmentUrl && (
+            <img src={nweetObj.attachmentUrl} width="50px" height="50px" />
+          )}
           {isOwner && (
             <>
               <button onClick={onDeleteClick}>Delete Nweet</button>
@@ -54,5 +55,4 @@ const Nweet = ({ nweetObj, isOwner }) => {
     </div>
   );
 };
-
 export default Nweet;
--- src/fbase.js
+++ src/fbase.js
@@ -2,6 +2,8 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import "firebase/storage";
+
 
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -11,11 +13,13 @@ import "firebase/firestore";
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseUrl: process.env.REACT_APP_DATABASE_URL,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID,
-  };
+  
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -23,5 +27,7 @@ firebase.initializeApp(firebaseConfig);
 export const firebaseInstance = firebase;
 export const authService = firebase.auth();
 export const dbService = firebase.firestore();
+export const storageService = firebase.storage();
+
 
 
